a5850c66d0ec801d1027ecf04077259134db4923
MartinInFrench <martinolivier1712@gmail.com>
2024-05-26 19:09:36 +0200

    ^^

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from pynput.mouse import Controller as MouseController, Button
 from pynput.keyboard import Controller as KeyboardController, Key, Listener
 import time
 from random import *
-from PIL import Image, ImageChops
-import pyautogui
 
 letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R", "S","T","U","V","W","X","Y","Z"]
 numbers = ["0","1","2","3","4","5","6","7","8","9"]
@@ -13,53 +11,7 @@ mouse = MouseController()
 
 
 Run = True
-
-
-def capture_screen(screen_number=1):
-    """
-    Capture the specified screen.
-    Note: This function assumes a setup where the screens are not mirrored and are side by side.
-    """
-    screens = pyautogui.screenshot().size  # Get the size of the screenshot (entire screen area)
-    screen_width, screen_height = screens[0] // 2, screens[1]  # Assume two screens of equal width
-    if screen_number == 1:
-        left = 0
-        top = 0
-        right = screen_width
-        bottom = screen_height
-    elif screen_number == 2:
-        left = screen_width
-        top = 0
-        right = screens[0]
-        bottom = screen_height
-    else:
-        raise ValueError("Invalid screen number. Only screen 1 or 2 is supported.")
-
-    return pyautogui.screenshot(region=(left, top, right - left, bottom - top))
-
-
-def is_image_on_screen(image_path, screen_number=1):
-    """
-    Check if the given image is present on the specified screen.
-
-    :param image_path: Path to the image file to check.
-    :param screen_number: The screen number to capture and check.
-    :return: True if the image is found on the screen, False otherwise.
-    """
-    screen = capture_screen(screen_number)
-    screen = screen.convert('RGB')
-    target_image = Image.open(image_path).convert('RGB')
-
-    screen_width, screen_height = screen.size
-    img_width, img_height = target_image.size
-
-    for x in range(screen_width - img_width + 1):
-        for y in range(screen_height - img_height + 1):
-            box = (x, y, x + img_width, y + img_height)
-            region = screen.crop(box)
-            if ImageChops.difference(region, target_image).getbbox() is None:
-                return True
-    return False
+generatedCode = int(input("Combien de code à tester ?"))
 
 def Launch():
     writeFile = open('code.txt', "a+")
@@ -103,8 +55,11 @@ def Launch():
 
     writeFile.close()
     time.sleep(1)
-    b = is_image_on_screen("ressource/img.png",1)
-    print(code +"  :  "+b)
+    print(code)
+    global generatedCode
+
+    generatedCode = generatedCode - 1
+    Run = inverse(generatedCode == 0)
 
     if Run:
         mouse.position = (922, 836)
@@ -112,25 +67,15 @@ def Launch():
         mouse.release(Button.left)
         keyboard.press(Key.f5)
         keyboard.release(Key.f5)
-
-def on_press(key):
-
-    if key == Key.f6:
-        Run = False
-    if key == Key.f5:
-        Run = True
         Launch()
 
 def inverse(bool):
-    if bool == True:
+    if bool:
         return False
     else:
         return True
 def main():
-    if inverse(Run):
-        exit()
-    with Listener(on_press=on_press) as listener:
-        listener.join()
+    Launch()
 
 if __name__ == "__main__":
     main()
